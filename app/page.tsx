3c8a580629ebad31cbd816d179bb7e9398935deb
Shadowfear36 <dylanrhinehart@gmail.com>
2024-11-27 19:56:16 -0800

    updates 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Stonkers from './images/stonkers.png';
-
+import Image from 'next/image';
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen overflow-hidden bg-[#573625] text-white">
-      <img src={Stonkers.src} alt="Stonkers Logo" className=" max-w-lg mb-[-180px]" />
+      <Image
+        src={Stonkers.src}
+        alt="Stonkers Logo"
+        width={500}
+        height={300}
+        className=" max-w-lg mb-[-180px]"
+      />
+      {/* eslint-disable @next/next/no-img-element */
+      /* eslint-disable react/no-unescaped-entities */}
       <h5 className="text-5xl mb-4 text-center font-snickers">You're Not You When You're Broke!</h5>
       <button className="px-4 py-2 text-white font-snickers text-4xl rounded-md hover:bg-[#e20d1b] border border-[#e20d1b]">
         Buy Stonkers
