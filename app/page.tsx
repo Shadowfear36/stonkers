673ae0fbd925e2813d7abc5161982ad7728fb92d
Shadowfear36 <dylanrhinehart@gmail.com>
2024-11-27 20:45:13 -0800

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
 import Image from 'next/image';
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen overflow-hidden bg-[#573625] text-white">
       <Image
-        src='/images/Stonkers.png'
+        src="/images/Stonkers.png"
         alt="Stonkers Logo"
         width={500}
         height={300}
-        className=" max-w-lg mb-[-180px]"
+        className="w-48 h-auto sm:w-64 md:w-80 lg:w-96 mb-[-50px] sm:mb-[-70px] md:mb-[-100px] lg:mb-[-120px]" // Responsive sizing and margin
       />
       {/* eslint-disable @next/next/no-img-element */
       /* eslint-disable react/no-unescaped-entities */}
-      <h5 className="text-5xl mb-4 text-center font-snickers">You're Not You When You're Broke!</h5>
-      <button className="px-4 py-2 text-white font-snickers text-4xl rounded-md hover:bg-[#e20d1b] border border-[#e20d1b]">
+      <h5 className="text-3xl sm:text-4xl md:text-5xl mb-4 text-center font-snickers">
+        You're Not You When You're Broke!
+      </h5>
+      <button className="px-3 py-1 sm:px-4 sm:py-2 text-lg sm:text-2xl md:text-4xl rounded-md hover:bg-[#e20d1b] border border-[#e20d1b] font-snickers">
         Buy Stonkers
       </button>
     </div>
